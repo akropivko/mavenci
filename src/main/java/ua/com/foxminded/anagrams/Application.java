ea75550eda56d5de0e9d26a444859c824eb0a060
Alexander Kropyvko <akropivko@gmail.com>
2020-06-02 14:04:00 +0300

    some changes

--- src/main/java/ua/com/foxminded/anagrams/Application.java
+++ src/main/java/ua/com/foxminded/anagrams/Application.java
@@ -5,7 +5,7 @@ public class Application {
     public static void main(String[] args) {
 
         final String[] STRINGS_TO_TEST = { "a1bcd efg!h", "DQ2w3eec 4r5t6y3e", 
-                                            "FQ2w3e4r 6yt7u8ie", "   ", null,
+                                            "FQ2w3e4r 6yt7u8ie", "   ", "", null,
                                             "qq22ww33 3e44rrTT" };
 
         Anagrams anagram = new Anagrams();
